1f5b96796b99f80e1a84ad5809377ae498556391
RobMDoe <RobMDoe@users.noreply.github.com>
2018-03-26 08:42:49 +1000

    Initial Projec Setup

---
+++ urbSim/urbSimCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace urbSim
+{
+    public class urbSimCommand : Command
+    {
+        public urbSimCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static urbSimCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "urbSimCommand"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: start here modifying the behaviour of your command.
+            // ---
+            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
+
+            
+            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
+
+            // ---
+
+            return Result.Success;
+        }
+    }
+}
